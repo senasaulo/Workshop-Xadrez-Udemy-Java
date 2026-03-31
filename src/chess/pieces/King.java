cfa989ca233695f9e81b857dc4a2f7c6c626f500
senasaulo <srs3@aluno.ifal.edu.br>
2026-03-31 12:25:00 -0300

    Movimentos do Rei

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -14,12 +15,30 @@ public class King extends ChessPiece{
 	public String toString() {
 		return "K";
 	}
+	
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		Position p = new Position(0,0);
+				
+		for (int i=-1; i<=1 ; i++) {
+			for(int j=-1; j<=1 ; j++) {
+				
+				if (i == 0 && j == 0) {
+					continue;
+				}
+				
+				p.setValues(position.getRow() + i, position.getColumn() + j);
+				if(getBoard().positionExists(p) && canMove(p)){
+					mat[p.getRow()][p.getColumn()] = true;
+				}	
+			}
+		}	
 		return mat;
-	}
-	
-
+	 }
 }
