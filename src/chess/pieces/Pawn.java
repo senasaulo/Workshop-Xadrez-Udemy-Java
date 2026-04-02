526d334333054539156ac8640491da28125da42b
senasaulo <srs3@aluno.ifal.edu.br>
2026-04-02 11:27:48 -0300

    Movimento xadrez En Passant

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -2,13 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Pawn extends ChessPiece {
 
-	public Pawn(Board board, Color color) {
+	private ChessMatch chessMatch;
+	
+	public Pawn(Board board, Color color,ChessMatch chessMatch) {
 		super(board, color);
+		this.chessMatch = chessMatch;
 	}
 
 	@Override
@@ -18,8 +22,7 @@ public class Pawn extends ChessPiece {
 
 	@Override
 	public boolean[][] possibleMoves() {
-		int i = 0;
-		int x = 0;
+		int i= 0 ,x = 0, z = 0;
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		
 		Position p = new Position(0,0);
@@ -27,10 +30,12 @@ public class Pawn extends ChessPiece {
 		if(getColor() == Color.WHITE) {
 			i = -1;
 			x = -2;
+			z = 3;
 		}
 		else {
 			i = 1;
 			x = 2;
+			z = 4;
 		}
 		
 		p.setValues(position.getRow() + i, position.getColumn());
@@ -49,6 +54,17 @@ public class Pawn extends ChessPiece {
 		p.setValues(position.getRow() + i, position.getColumn() - i);
 		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
+		}
+		if(position.getRow() == z) {
+			Position left = new Position(position.getRow(), position.getColumn() + 1);
+			if(getBoard().positionExists(left) && isThereOpponentPiece(left) && getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
+				mat[left.getRow() + i][left.getColumn()]= true;
+			}
+			Position right = new Position(position.getRow(), position.getColumn() - 1);
+			if(getBoard().positionExists(right) && isThereOpponentPiece(right) && getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
+				mat[left.getRow() + i][left.getColumn()]= true;
+			}
+						
 		}
 
 		return mat;
